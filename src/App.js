89dd3cea6493cd02a1a9d0fd40fb376deb94af8f
Carlos Lima <carlos0ficial500@gmail.com>
2024-01-02 18:56:39 -0300

    Adiciona repositório dedicado aos meus estudos fundamentais sobre React.
    Neste primeiro commit, explorei a estrutura inicial do Babel e Webpack, e criei meu primeiro componente neste repositório. O objetivo principal é aprofundar meu entendimento sobre o funcionamento interno do React.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App(){
+    return (
+        <>
+        <h1>Oi eu sou um componente</h1>
+        <h2>Subtitulo</h2>
+        </>
+    )
+  }
+
+export default App;  
